630aec986e49c25615cab7122ee57eca865fa032
Rehan <rehanlemu@gmail.com>
2025-07-27 01:18:03 +0600

    added navbar menu item with unique hover effect

--- components/ourcomponents/HeaderMenu.tsx
+++ components/ourcomponents/HeaderMenu.tsx
@@ -1,9 +1,23 @@
+import { headerData } from '@/constants/data'
+import Link from 'next/link'
 import React from 'react'
 
 const HeaderMenu = () => {
     return (
-        <div>
-            
+        <div className='hidden md:inline-flex w-1/3 items-center gap-7 text-sm font-semibold capitalize text-lightText'>
+            {
+                headerData?.map((item) =>(
+                    <Link 
+                        key={item?.title} 
+                        href={item.href} 
+                        className=" hover:text-darkRed hoverEffect relative group"
+                    >
+                        {item.title}
+                        <span className='absolute  -bottom-0.5 w-0 h-0.5 left-1/2 bg-darkRed group-hover:w-1/2 group-hover:left-0 hoverEffect' />
+                        <span className='absolute  -bottom-0.5 w-0 h-0.5 right-1/2 bg-darkRed group-hover:w-1/2 group-hover:right-0 hoverEffect' />
+                    </Link>
+                ))
+            }
         </div>
     )
 }
