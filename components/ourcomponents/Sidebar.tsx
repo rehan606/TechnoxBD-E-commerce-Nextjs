e62a0b772da8fbf6a17957cfde4ed60e28a24ef1
Rehan <rehanlemu@gmail.com>
2025-07-29 00:20:59 +0600

    added menu item in sidebar menu

--- components/ourcomponents/Sidebar.tsx
+++ components/ourcomponents/Sidebar.tsx
@@ -1,5 +1,9 @@
+"use client"
+import { headerData } from '@/constants/data'
 import React from 'react'
 import Logo from './Logo'
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
 
 interface SidebarProps {
   isOpen: boolean;  // Prop to control the visibility of the sidebar
@@ -7,6 +11,7 @@ interface SidebarProps {
 }
 
 const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose }) => {
+    const pathname = usePathname();
     return (
         <div className={`fixed inset-y-0 left-0 z-50 bg-darkColor/50 w-full shadow-xl transform  ${isOpen ? 'translate-x-0' : '-translate-x-full '} hoverEffect`}>
 
@@ -14,8 +19,24 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose }) => {
             <div className="min-w-72 max-w-96 bg-black h-full text-white p-10 border-r border-r-darkRed flex flex-col gap-6">
                 <div className="flex items-center justify-between">
                     <Logo className="text-white" subClass="group-hover:text-white"/>
-                    <button onClick={onClose} className="hover:text-darkRed hoverEffect border border-gray-100 py-1 px-3 ">X</button>
+                    <button onClick={onClose} className="hover:text-darkRed hoverEffect border border-gray-100 py-1 px-3 ">X</button>   
                 </div>
+
+                <div className="flex flex-col gap-4 text-base font-semibold capitalize text-white/80 tracking-wide">
+                    {headerData.map((item) => (
+                        <Link
+
+                            key={item?.title} 
+                            href={item?.href} 
+                            className={`hover:text-darkRed hoverEffect relative ${pathname === item?.href && "text-darkRed"}`}
+                            onClick={onClose}  // Close the sidebar when a link is clicked
+                            >
+                            {item?.title}
+                            
+                        </Link>
+                    ))}
+                </div>
+
             </div>
 
         </div>
