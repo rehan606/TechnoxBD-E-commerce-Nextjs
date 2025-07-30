e9de6cb69377580d95716751f7c16d1a93abfe3b
Rehan <rehanlemu@gmail.com>
2025-07-30 22:44:27 +0600

    Login authentication using Clerk

--- components/ourcomponents/Header.tsx
+++ components/ourcomponents/Header.tsx
@@ -6,8 +6,14 @@ import SearchBar from './SearchBar'
 import CartIcon from './CartIcon'
 import FavouriteIcon from './FavouriteIcon'
 import MobileMenu from './MobileMenu'
+import { auth, currentUser } from '@clerk/nextjs/server'
+import { UserIcon } from 'lucide-react'
+import { ClerkLoaded, SignedIn, SignInButton, UserButton } from '@clerk/nextjs'
 
-const Header = () => {
+const Header = async() => {
+
+  const user = await currentUser();
+
   return (
     <header className="bg-white border-b border-gray-200 shadow-sm py-5 ">
         <Container className='flex items-center justify-between gap-7 text-lightText'>
@@ -23,7 +29,17 @@ const Header = () => {
                 <SearchBar/>
                 <FavouriteIcon/>
                 <CartIcon/>
-                <button className='bg-darkRed text-white rounded-md py-2 px-4 hover:bg-darkRed/80'>login</button>
+
+
+                {/* Signin and signOut With Clerk */}
+                <ClerkLoaded>
+                  <SignedIn>
+                    <UserButton/>  
+                  </SignedIn>
+                  { 
+                    !user && <SignInButton mode='modal'><button className='bg-darkRed text-sm font-semibold hover:text-darkColor text-white rounded-md py-2 px-4 hover:bg-darkRed/80 cursor-pointer'>login</button></SignInButton>
+                  }
+                </ClerkLoaded>
             </div>
         </Container>
     </header>
